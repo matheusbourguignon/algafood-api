8336b7f8644014321df3a4ba8a862050238c5320
matheusbourguignon <matheusmbl21@gmail.com>
2025-01-27 16:00:49 -0300

    Ambiguidade de beans e injenção de lista de beans

--- src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -5,28 +5,20 @@ import com.algaworks.algafood.notification.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired(required = false)
-    private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-
-    }
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        if(notificador != null) {
+        for (Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        }else {
-            System.out.println("Não existe notificador, mas o cliente foi ativado!");
         }
     }
 
-    public void setNotificador(Notificador notificador) {
-        this.notificador = notificador;
-    }
 }
