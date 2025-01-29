e10c2f04f5e2b2a596277132a05cb20e4ec2f9fb
matheusbourguignon <matheusmbl21@gmail.com>
2025-01-29 18:17:13 -0300

    Acessando propriedades com configuration properties

--- src/main/java/com/algaworks/algafood/notification/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notification/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.notification;
 
 import com.algaworks.algafood.user.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +15,15 @@ import org.springframework.stereotype.Component;
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-    @Value("${notificador.email.porta-servidor}")
-    private Integer porta;
+
+    @Autowired
+    private NotificadorProperties properties;
+
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("HosT: " + host);
-        System.out.println("Porta " + porta);
+        System.out.println("Host: " + properties.getHostServidor());
+        System.out.println("Porta " + properties.getPortaServidor());
         System.out.printf("Notificando %s atráves do e-mail %s: %s\n", cliente.getNome(),
                 cliente.getEmail(), mensagem);
     }
---
+++ src/main/java/com/algaworks/algafood/notification/NotificadorProperties.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.notification;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("notificador.email")
+public class NotificadorProperties {
+
+    // host do servidor e-mail.
+    private String hostServidor;
+
+    // porta do servidor e-mail.
+    private Integer portaServidor = 30;
+
+    public String getHostServidor() {
+        return hostServidor;
+    }
+
+    public void setHostServidor(String hostServidor) {
+        this.hostServidor = hostServidor;
+    }
+
+    public Integer getPortaServidor() {
+        return portaServidor;
+    }
+
+    public void setPortaServidor(Integer portaServidor) {
+        this.portaServidor = portaServidor;
+    }
+}
