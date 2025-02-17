30fe9a56f35c8de94313f796f8ac4d8485323268
matheusbourguignon <matheusmbl21@gmail.com>
2025-02-17 10:01:57 -0300

    Conhecendo e usando o lombok

--- src/main/java/com/algaworks/algafood/entity/Cozinha.java
+++ src/main/java/com/algaworks/algafood/entity/Cozinha.java
@@ -1,43 +1,22 @@
 package com.algaworks.algafood.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
-
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
-
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Cozinha cozinha)) return false;
-        return Objects.equals(getId(), cozinha.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
+
+
+
