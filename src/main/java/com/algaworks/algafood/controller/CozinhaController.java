d8bbe42a47a35df5c27faf4041e1e7df34a0546f
matheusbourguignon <matheusmbl21@gmail.com>
2025-02-25 12:36:55 -0300

    Modelando e requisitando um collection resource com GET

---
+++ src/main/java/com/algaworks/algafood/controller/CozinhaController.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.controller;
+
+import com.algaworks.algafood.entity.Cozinha;
+import com.algaworks.algafood.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
